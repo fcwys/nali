bc5375865a42d5026a3dc535b1948773cf115f5b
风吹我已散 <fcwys@qq.com>
2022-05-26 15:07:03 +0800

    更新帮助提示

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,10 @@
 package cmd
 
 import (
-	"net/http"
+	"log"
 	"strings"
 
-	"github.com/labstack/echo/v4"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"github.com/zu1k/nali/pkg/entity"
 )
@@ -40,19 +40,23 @@ var serverCmd = &cobra.Command{
 		port, _ := cmd.Flags().GetString("port")
 		port = ":" + port
 
-		//启动echo web服务
-		ec := echo.New()
+    log.Println(">>> Nali Server running on *"+port)
+    //关闭调试模式
+    gin.SetMode(gin.ReleaseMode)
+		//启动Gin服务
+		ec := gin.Default()
 		//参数方式查询
-		ec.GET("/", func(c echo.Context) error {
-			ip := c.QueryParam("ip")
+		ec.GET("/", func(c *gin.Context) {
+			ip := c.Query("ip")
 			if ip == "help" {
 				reinfo := &Errinfo{
 					Code: -1,
-					Msg:  "Param: ip=223.5.5.5 or /223.5.5.5 (Default: visitor ip)",
+          Msg:  "Param: /223.5.5.5 or ip=223.5.5.5 (Default: visitor ip)",
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			} else if ip == "" {
-				ip = c.RealIP()
+				ip = c.ClientIP()
 			}
 			//fmt.Println(ip)
 			args = nil
@@ -64,52 +68,58 @@ var serverCmd = &cobra.Command{
 					Code: -1,
 					Msg:  "No record, please try again",
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			} else {
 				reinfo := &Ipinfo{
 					Code: 1,
 					Ip:   res[0].Text,
 					Addr: strings.Replace(res[0].Info, "\t", " ", -1),
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			}
 
 		})
-		//路径方式查询
-		ec.GET("/:ip", func(c echo.Context) error {
+
+		//路径查询方式
+		ec.GET("/:ip", func(c *gin.Context) {
 			ip := c.Param("ip")
 			if ip == "help" {
 				reinfo := &Errinfo{
 					Code: -1,
-					Msg:  "Help: ?ip=223.5.5.5 or /223.5.5.5 (Default: your ip)",
+          Msg:  "Param: /223.5.5.5 or ip=223.5.5.5 (Default: visitor ip)",
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			} else if ip == "" {
-				ip = c.RealIP()
+				ip = c.ClientIP()
 			}
 			//fmt.Println(ip)
 			args = nil
 			args = append(args, ip)
 			res := entity.ParseLine(strings.Join(args, " "))
-			//addr := strings.Split(strings.Replace(res[0].Info, "\t", " ", -1), " ")
 			//判断是否有结果返回
 			if res[0].Info == "" {
 				reinfo := &Errinfo{
 					Code: -1,
 					Msg:  "No record, please try again",
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			} else {
 				reinfo := &Ipinfo{
 					Code: 1,
 					Ip:   res[0].Text,
 					Addr: strings.Replace(res[0].Info, "\t", " ", -1),
 				}
-				return c.JSON(http.StatusOK, reinfo)
+				c.JSON(200, reinfo)
+				return
 			}
 
 		})
-		ec.Logger.Fatal(ec.Start(port))
+		//ec.Logger.Fatal(ec.Start(port))
+		ec.Run(port)
 	},
 }
 
